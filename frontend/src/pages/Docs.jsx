4f277c44928fdc369ab8fd58d52fb4388b390da1
kxan1 <kimogingging@gmail.com>
2026-09-16 10:36:06 +0800

    Fixed manual link

--- frontend/src/pages/Docs.jsx
+++ frontend/src/pages/Docs.jsx
@@ -3,8 +3,8 @@ export default function Docs() {
     <div className="app-shell narrow">
       <h1 className="page-title">Documentation &amp; SOP</h1>
       <h3>
-        Here's the link to the manual and standard operating procedure (SOP):\n
-        https://docs.google.com/document/d/1oZDsfshWmWlwvHn6Jqt_pOE-awnmfryE1U_eEyaZDNA/edit?usp=sharing
+        Here's the link to the manual and standard operating procedure (SOP):
+        <a href="https://docs.google.com/document/d/1oZDsfshWmWlwvHn6Jqt_pOE-awnmfryE1U_eEyaZDNA/edit?usp=sharing">Pellet Monitor Manual and SOP</a>
       </h3>
       </div>
   )
